893dc64c5b418f582543ad292644b25aef2511da
kadirozlem <ozlem16@itu.edu.tr>
2023-03-08 11:43:48 +0300

    Device Status Added

--- UserInterface/Routes/Home.js
+++ UserInterface/Routes/Home.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const Config = require("../../Config");
 const router = express.Router();
+const shell = require('shelljs')
 
 router.get('/', function (req, res, next){
 
     res.render('index',{title:"FogETex", UserType:Config.DeviceTypes.UserInterface, DeviceInfo:req.app.FogETex.DeviceInfo,Directory: 'Home'})
+});
+
+router.get('/DeviceStatus', (req, res, next)=>{
+    res.json({Status: 'Running', DeviceType: req.app.FogETex.DeviceInfo.DeviceTypeString});
+});
+
+router.get('/UpdateDevice', (req, res,next)=>{
+    res.redirect('/DeviceStatus');
+    shell.exec('sh Update.sh')
 });
 
 module.exports = router;
