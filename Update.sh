fa89ad6285fffffc8f1399e17916c6a90d1444c4
kadirozlem <kadirozlem@hotmail.com>
2023-08-18 02:03:04 +0300

    Cloud Python Start Problem

--- Update.sh
+++ Update.sh
@@ -8,5 +8,5 @@ node ./Config/CacheDeviceType.js
 forever restart Server.js
 forever restart LoadTest/FogETexResourceManager/index.js
 cd GaitAnalysis
-forever restart -c python main.py
+forever restart -c python3 main.py
 cd ..
